44442cf8aa21442b54804ffed3f932e4916ce6ad
ameloft <ameloft@protonmail.com>
2025-03-10 01:37:02 +0200

    Add files via upload
    серверна частина месенджера

---
+++ server.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+clients = []
+
+def broadcast(message, client_socket):
+    for client in clients:
+        if client != client_socket:
+            try:
+                client.send(message)
+            except:
+                client.close()
+                clients.remove(client)
+
+def handle_client(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            broadcast(message, client_socket)
+        except:
+            client_socket.close()
+            clients.remove(client_socket)
+            break
+
+def start_server(host='0.0.0.0', port=1111):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+
+    print(f"Сервер запущено на {host}:{port}")
+
+    while True:
+        client_socket, client_address = server.accept()
+        print(f"Підключено {client_address}")
+        clients.append(client_socket)
+        thread = threading.Thread(target=handle_client, args=(client_socket,))
+        thread.start()
+
+if __name__ == "__main__":
+    start_server()
